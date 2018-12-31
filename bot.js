bd08d811839847870aca686e209bcc185ab22e8a
خالد الرشيدي <42046412+khaliid4@users.noreply.github.com>
2019-01-01 02:40:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1065,8 +1065,7 @@ client.on('message', function(msg) {
 client.on('message', message => {
      if (message.author.bot) return;
     if (message.content.startsWith("-help")) {
- if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-		 message.channel.send('**تم ارسال الأوامر لك بالخاص مع تمنياتي لك بالتوفيق والنجاح**');
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );خاص مع تمنياتي لك بالتوفيق والنجاح**')
             
 	
 		 
