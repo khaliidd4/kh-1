21264ce0b83beda394b790c266244dff0d5d431e
خالد الرشيدي <42046412+khaliid4@users.noreply.github.com>
2019-01-01 14:11:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -951,7 +951,7 @@ client.on('ready',async () => {
     const config = {
       name: "RTF DisMTA", // اسم الرتبة
       guildid: "463576669199007754", // اي دي السيرفر
-      sec: 0.01 // عدد الثواني
+      sec: 0.1 // عدد الثواني
     };
     let guild = client.guilds.get(config.guildid);
     let role = guild.roles.find(role => role.name === config.name);
